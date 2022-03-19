c08a91f3f813accc1f2fd5b6fead9823a7380c1a
amenson1983 <75535151+amenson1983@users.noreply.github.com>
2022-03-19 18:52:02 +0200

    complex mapping half

--- __main__.py
+++ __main__.py
@@ -3,6 +3,7 @@ from fuzzywuzzy import process
 import pandas as pd
 import numpy as np
 import jellyfish as jf
+import string as st
 from openpyxl import load_workbook
 
 class CFunctions:
@@ -13,9 +14,24 @@ class CFunctions:
             matched_value = s[0]
         return matched_value
 
+    def intermediate_changed_list(self,string,unnecessary_symbols_list):
+        mapping_item_dictionary = {}
+        changed_string = []
+        for symb in unnecessary_symbols_list:
+            changed_string_ = string.lower()
+            changed_string = changed_string_.split(symb)
+            changed_string = ''.join(changed_string)
+            changed_string = changed_string.translate({ord(symb):None})
+        for symb in unnecessary_symbols_list:
+            changed_string = changed_string.translate({ord(symb): None})
+
+        mapping_item_dictionary.update({changed_string:string})
+        return changed_string,mapping_item_dictionary
+
 class COperations:
     def list_correction_to_ethalon_naming_list(self,incoming_list,ethalon_naming_list,percent):
         corrected_list, problematic_items = [], {}
+
         for item in incoming_list:
             value = f.item_match_in_list_by_percent(item, ethalon_naming_list, percent)
             if value == '':
@@ -33,12 +49,33 @@ f = CFunctions()
 o = COperations()
 if __name__ == '__main__':
 
-    incoming_list = ['praguee','kiev','kopenhagen','stock_gohlm','paris','berlin','Berdichev']
+    incoming_list = ['prague_e,','kiev','kopenhagen','stock_gohlm','paris ','berlin','Berdichev']
     ethalon_naming_list = ['Berlin','Kyiv','Prague','Kopenhagen','Paris','Stockgohlm','Berdychiv']
-    percent = 75
+    percent = 80
+    unnecessary_symbols_list = ["№","_","%","-","/","|",",",".",".",",","!"," "]
 
-    corrected_list,problematic_items = o.list_correction_to_ethalon_naming_list(incoming_list,ethalon_naming_list,percent)
-    print(f"Corrected list with applied actual percent threshold {percent}%: \n{corrected_list}")
+    '''corrected_list,problematic_items = o.list_correction_to_ethalon_naming_list(incoming_list,ethalon_naming_list,percent)
+    print(f"Corrected list with applied actual percent threshold {percent}%: \n{corrected_list}")'''
+
+    df = pd.read_excel('test.xlsx',engine='openpyxl')
+    incoming_list_1 = df['item_sales_report'].values
+    ethalon_naming_list_1 = df['item_kpi_report'].values
+    mapping_item_dictionary_incoming = {}
+    for item in incoming_list_1:
+        changed_string,mapping_item_dictionary = f.intermediate_changed_list(item,unnecessary_symbols_list)
+        mapping_item_dictionary_incoming.update(mapping_item_dictionary)
+        print(f"{mapping_item_dictionary_incoming.get(changed_string)} -  {changed_string}")
+
+
+
+
+    '''corrected_list, problematic_items = o.list_correction_to_ethalon_naming_list(incoming_list_1, ethalon_naming_list_1,
+                                                                                 percent)
+    df['result'] = corrected_list
+    df.to_excel('result.xlsx',engine='openpyxl',index=False)
+    os.startfile('result.xlsx')
+    print(f"Corrected list with applied actual percent threshold {percent}%: \n{corrected_list}")'''
+
 
 
 
